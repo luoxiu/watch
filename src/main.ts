805af1596f92cb16db266af530e14809145246c8
luoxiu <luoxiu.dev@gmail.com>
2022-02-28 23:18:38 +0800

    no top await

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,19 @@ import { watchAndRewatchOnConfigChange } from "./watch.js";
 import chalk from "chalk";
 import "zx/globals";
 
-const program = new Command();
+(async () => {
+  const program = new Command();
 
-const pkg = await readPKG();
+  const pkg = await readPKG();
 
-program
-  .version(pkg.version, "-v, --version", "display version")
-  .action(async () => {
-    await watchAndRewatchOnConfigChange();
-  })
-  .parseAsync(process.argv)
-  .catch(err => {
-    console.error(chalk.red(err));
-    process.exit(1);
-  });
+  program
+    .version(pkg.version, "-v, --version", "display version")
+    .action(async () => {
+      await watchAndRewatchOnConfigChange();
+    })
+    .parseAsync(process.argv)
+    .catch(err => {
+      console.error(chalk.red(err));
+      process.exit(1);
+    });
+})();
